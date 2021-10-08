813269791f3f7facc482f66b2e26b40cf28c06f8
briantwhelan <whelanb8@tcd.ie>
2021-10-08 22:34:39 +0100

    Add BinarySearchTree JUnit test class
    This commit adds the BinarySearchTreeTest.java file containing a JUnit
    test for the BinarySearchTree constructor.

---
+++ Java/BinarySearchTreeTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+/*************************************************************************
+ *  {@code BinarySearchTree} test class.
+ *
+ *  @version 08/08/21
+ *
+ *  @author Brian Whelan
+ *
+ *************************************************************************/
+public class BinarySearchTreeTest
+{	
+	/**
+     * Test {@code BinarySearchTree()} 
+     */
+	@Test
+	public void testConstructor()
+	{
+		new BinarySearchTree<Integer, Integer>();		     
+	}
+}
