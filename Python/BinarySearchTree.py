56deff59dd43cc51d615b547845bb367b96120b3
briantwhelan <whelanb8@tcd.ie>
2021-10-11 19:51:21 +0100

    Add basic BinarySearchTree data structure
    This commit adds the BinarySearchTree.py file containing a basic
    BinarySearchTree implementation as well as a number of function headers
    which still require implementations.

---
+++ Python/BinarySearchTree.py
@@ -0,0 +1,21 @@
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        
+    class Node:
+        def __init__(self, key, value, size):
+            self.key = key
+            self.value = value
+            self.size = size
+            self.left = None
+            self.right = None
+    
+    def size(self):
+        return 0
+
+    def put(self, key, value):
+        return 0
+
+    def __str__(self):
+        return ""      
+        
