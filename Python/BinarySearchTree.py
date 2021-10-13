19a99fe0d30c0e41717683237c93c81c639ce450
briantwhelan <whelanb8@tcd.ie>
2021-10-13 23:22:51 +0100

    Add implementations for unimplemented methods
    This commit adds implementations for the methods size(), put(key, value)
    and __str__(). These implementations pass all pyunit tests for their
    respective method.

--- Python/BinarySearchTree.py
+++ Python/BinarySearchTree.py
@@ -11,11 +11,44 @@ class BinarySearchTree:
             self.right = None
     
     def size(self):
-        return 0
+        return self.sizeR(self.root)
+
+    def sizeR(self, node):
+        size = 0
+        if(node != None):
+            size = node.size
+        return size
 
     def put(self, key, value):
-        return 0
+        if(value != None):
+            self.root = self.putR(self.root, key, value)
+
+    def putR(self, node, key, value):
+        if(node == None):
+            node = self.Node(key, value, 1)
+        else:
+            if(key < node.key):
+                node.left = self.putR(node.left, key, value)
+            elif(key > node.key):
+                node.right = self.putR(node.right, key, value)
+            else:
+                node.value = value
+            node.size = 1 + self.sizeR(node.left) + self.sizeR(node.right)
+        return node
 
     def __str__(self):
-        return ""      
-        
+        keysInOrder = ""
+        if(self.root == None):
+            keysInOrder = "()"
+        else:
+            keysInOrder = self.__str__R(self.root)
+        return keysInOrder
+
+    def __str__R(self, node):
+        keysInOrder = ""
+        if(node == None):
+            keysInOrder = "()"
+        else:            
+            keysInOrder = "(" + self.__str__R(node.left) + str(node.key) + self.__str__R(node.right) + ")"
+        return keysInOrder
+
