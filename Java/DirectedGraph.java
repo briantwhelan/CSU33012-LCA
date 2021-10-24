d902749e25d98822dd68dfec0d17fc4bf5b64528
briantwhelan <whelanb8@tcd.ie>
2021-10-24 21:30:10 +0100

    Add implementations for constructor and addEdge method
    This commit adds implementations for the constructor and the addEdge(int
    vertex1, int vertex2) method. Both of these implementations pass all
    their respective JUnit tests. Javadoc comments have also been added.

--- Java/DirectedGraph.java
+++ Java/DirectedGraph.java
@@ -15,31 +15,81 @@ public class DirectedGraph
     private int numberOfEdges;
     private HashSet<Integer>[] adjacencyLists;
 
+	/**
+     * Create an empty directed graph with a particular number of vertices
+     * @param: number of vertices to be included in directed graph
+     */
     public DirectedGraph(int numberOfVertices) throws IllegalArgumentException
     {
-        this.numberOfVertices = numberOfVertices;
+       if(numberOfVertices < 0)
+		{
+			throw new IllegalArgumentException("Number of vertices must be positive");
+		}
+		
+		this.numberOfVertices = numberOfVertices;
+		this.numberOfEdges = 0;
+		this.adjacencyLists = (HashSet<Integer>[]) new HashSet[numberOfVertices];
+		for(int vertex = 0; vertex < numberOfVertices; vertex++)
+		{
+			adjacencyLists[vertex] = new HashSet<Integer>();
+		}
     }
 
+	/**
+     * Add an edge to the directed graph
+     * 
+     * @param vertex1: start vertex of edge to be added
+     * @param vertex2: end vertex of edge to be added
+     */
     public void addEdge(int vertex1, int vertex2)
     {
-
+		if((vertex1 >= 0) && (vertex1 < numberOfVertices)
+    		&& (vertex2 >= 0) && (vertex2 < numberOfVertices)
+    		&& (!adjacencyLists[vertex1].contains(vertex2)))
+    	{
+	    	adjacencyLists[vertex1].add(vertex2);
+	    	numberOfEdges++;
+    	}
     }
 
+	/**
+     * Returns the Lowest Common Ancestor of the two specified vertices (if they exist) in the {@code DirectedGraph}.
+     *
+     * @param vertex1 the first vertex
+     * @param vertex2 the second vertex
+     * @return the {@code int} corresponding to the Lowest Common Ancestor vertex of {@code vertex1} and {@code vertex2} (or {@code null} if no such vertex exists)
+     */
     public int lowestCommonAncestor(int vertex1, int vertex2)
     {
         return 0;
     }
 
+	 /**
+     * Gets the number of vertices in the {@code DirectedGraph}
+     * 
+     * @return the number of vertices in the {@code DirectedGraph}
+     */
     public int getNumberOfVertices()
     {
         return numberOfVertices;
     }
 
+	 /**
+     * Gets the number of edges in the {@code DirectedGraph}
+     * 
+     * @return the number of edges in the {@code DirectedGraph}
+     */
     public int getNumberOfEdges()
     {
         return numberOfEdges;
     }
 
+	/**
+     * Gets the {@code String} representation of the {@code DirectedGraph}.
+     * 
+     * @return the {@code String} representation of the {@code DirectedGraph}
+     */
+	@Override
     public String toString()
     {
         return "";
