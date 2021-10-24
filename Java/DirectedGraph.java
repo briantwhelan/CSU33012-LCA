a5cd36dfcf38b22c5a5e2b63b9e35ff6ab527ec6
briantwhelan <whelanb8@tcd.ie>
2021-10-24 20:31:57 +0100

    Add basic DirectedGraph data structure with unimplemented test class
    This commit replaces the BinarySearchTree.java and
    BinarySearchTreeTest.java files with the DirectedGraph.java and
    DirectedGraphTest.java respectively. This change was made after
    reviewing the updated assignment specification which required the LCA
    algorithm to work on directed acyclic graphs - which could not be
    represented in the BinarySearchTree data structure that was previously
    implemented. Hence, I've chosen to discard the BinarySearchTree approach
    and instead work on a more appropriate data structure. The methods
    defined in DirectedGraph.java still require implementations and the test
    class requires tests for these methods.

---
+++ Java/DirectedGraph.java
@@ -0,0 +1,37 @@
+/*************************************************************************
+ *  {@code DirectedGraph} class.
+ *  Adapted from Sedgewick and Wayne's Algorithms Textbook.
+ *  @see <a href="https://algs4.cs.princeton.edu/">Algorithms Textbook</a>
+ *
+ *  @version 24/10/21
+ *
+ *  @author Brian Whelan
+ *
+ *************************************************************************/
+public class DirectedGraph 
+{
+    private final int numberOfVertices;
+    private int numberOfEdges;
+    private HashSet<Integer>[] adjacencyLists;
+
+    public DirectedGraph(int numberOfVertices) throws IllegalArgumentException
+    {
+
+    }
+
+    public void addEdge(int vertex1, int vertex2)
+    {
+
+    }
+
+    public int lowestCommonAncestor(int vertex1, int vertex2)
+    {
+        return 0;
+    }
+
+    public String toString()
+    {
+        return "";
+    }
+}
+
---
+++ Java/DirectedGraphTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+/*************************************************************************
+ *  {@code DirectedGraph} test class.
+ *
+ *  @version 24/10/21
+ *
+ *  @author Brian Whelan
+ *
+ *************************************************************************/
+public class DirectedGraphTest
+{   	
+    /**
+     * Test {@code DirectedGraphTest} 
+     */
+    @Test
+    public void testConstructor()
+    {
+    		     
+    }
+
+    /**
+     * Test {@code lowestCommonAncestor(int vertex1, int vertex2)}
+     */
+    @Test
+    public void testLowestCommonAncestor()
+    {
+        
+    }
+
+    /**
+     * Test {@code toString()} 
+     */
+    @Test
+    public void testToString() 
+    {
+
+    }
+}
+
